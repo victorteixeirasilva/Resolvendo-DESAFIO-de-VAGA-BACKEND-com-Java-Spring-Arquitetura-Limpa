28f759d1b061a6c924c010c423001a086d1afefb
Victor Teixeira Silva <105030970+victorteixeirasilva@users.noreply.github.com>
2025-03-27 13:17:00 -0300

    Update EmailSenderController.java

--- email-service/src/main/java/com/victorteixeriasilva/email_service/controllers/EmailSenderController.java
+++ email-service/src/main/java/com/victorteixeriasilva/email_service/controllers/EmailSenderController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "Email Sander", description = "Gerenciamento de Envio de Emails")
 @RestController
 @RequestMapping("/api/email")
 public class EmailSenderController {
@@ -22,6 +25,11 @@ public class EmailSenderController {
         this.emailSanderService = emailSanderService;
     }
 
+    @Operation(
+            summary =
+                    "Enviando um email",
+            description =
+                    "Retorna uma mensagem informando se o e-mail foi enviado ou não com sucesso!")
     @PostMapping()
     public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request){
         try {
